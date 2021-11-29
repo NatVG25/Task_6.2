d6334e6637b6a9032ab070688e3bbb6639da1920
Goliash <Goliash@KONSTRUKTOR4>
2021-11-29 18:14:19 +0500

    Добавьте файлы проекта.

---
+++ Task_6.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string text = Console.ReadLine(); //ввод предложения с клавиатуры
+            string textLower = text.ToLower();//убираем регистр
+            string textReplace = textLower.Replace(" ", ""); //убираем пробелы
+            char[] sArrayOut = textReplace.ToCharArray(); //преобразуем строку в массив
+            Array.Reverse(sArrayOut); //реверс массива
+            string result = new string(sArrayOut); //генерируем новую строку
+
+            bool check = String.Equals(textReplace, result); //проверяем условие на палиндром
+            Console.WriteLine();
+            if (check == true)
+            {
+                Console.WriteLine("Строка является палиндромом");
+            }
+            else
+            {
+                Console.WriteLine("Строка не является палиндромом");
+            }
+                      
+                Console.ReadKey();
+
+        }
+    }
+}
